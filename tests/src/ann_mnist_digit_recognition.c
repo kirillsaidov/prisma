5b3d2b14199009b4dfeb4e68e8a26e206e77c2bb
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-10-23 21:41:56 +0600

    update gitignore

---
+++ tests/src/ann_mnist_digit_recognition.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+//
+
+void run_ann_mnist_digit_recognition(void) {
+    vt_debug_redirect_output("debug.log");
+
+    VT_LOG_INFO("Looking for MNIST digits dataset...");
+    if (vt_path_exists("assets/cache"))
+}
+
